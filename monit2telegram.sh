76ed5291329ad49dcf19052b88ae9c9f4ab2790c
nerdsav <nerdsav@gmail.com>
2021-07-06 12:56:03 +0300

    Update monit2telegram.sh

--- monit2telegram.sh
+++ monit2telegram.sh
@@ -4,4 +4,4 @@
 #
 # Depends on having `/usr/local/bin/sendtelegram` installed and a config file in `/etc/telegramrc`
 #
-/usr/local/bin/sendtelegram -c /etc/telegramrc -m "Monit $MONIT_SERVICE - $MONIT_EVENT at $MONIT_DATE on $MONIT_HOST: $MONIT_ACTION $MONIT_DESCRIPTION."
+/usr/local/bin/sendtelegram -v -c /etc/telegramrc -m "[`hostname`] MONIT ($MONIT_SERVICE) - $MONIT_EVENT at $MONIT_DATE on $MONIT_HOST: $MONIT_ACTION $MONIT_DESCRIPTION."
